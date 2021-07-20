86886a4a2e012dd240cb47faf8fd10dd0b1c690e
koliv5936@gmail.com <koliv5936@gmail.com>
2021-07-20 22:03:23 +0530

    added app folder

---
+++ app/src/main/java/in/gmail/koliv5936/todoapp/NoteDatabase.kt
@@ -0,0 +1,33 @@
+package `in`.gmail.koliv5936.todoapp
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Note::class), version = 1,exportSchema = false)
+abstract class NoteDatabase : RoomDatabase(){
+
+    abstract fun getNoteDao(): NoteDao
+
+    companion object{
+
+        @Volatile
+        private var INSTANCE: NoteDatabase? = null
+
+        fun getDatabase(context: Context): NoteDatabase{
+            //If INSTANCE is not null ,then return it,
+            //if it is null , then create the database
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NoteDatabase::class.java,
+                    "note_database"
+                ).build()
+                INSTANCE = instance
+                //return instance
+                instance
+            }
+        }
+    }
+}
